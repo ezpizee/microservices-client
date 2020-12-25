5ab71f5e0193cab3e044e10ba107518977dca1ba
pangkhon <pang.khon@ezpizee.com>
2020-12-25 14:09:19 +0700

    updat request put method

--- src/Ezpizee/MicroservicesClient/Client.php
+++ src/Ezpizee/MicroservicesClient/Client.php
@@ -110,7 +110,7 @@ class Client
         $response->setUnirestResponse($unirestRequest->raw_body);
         break;
       case $this->methods['put']:
-        $unirestRequest = Request::delete($url, $this->headers, $this->body);
+        $unirestRequest = Request::put($url, $this->headers, $this->body);
         $response->setUnirestResponse($unirestRequest->raw_body);
         break;
       case $this->methods['patch']:
