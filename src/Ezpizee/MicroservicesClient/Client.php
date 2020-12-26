c2ce9ab38737aaf2ed2ae96652bbbf57bc94caeb
Sothea Nim <sothea.nim@ezpizee.com>
2020-12-25 20:14:08 -0500

    improved

--- src/Ezpizee/MicroservicesClient/Client.php
+++ src/Ezpizee/MicroservicesClient/Client.php
@@ -7,6 +7,32 @@ use Unirest\Request;
 
 class Client
 {
+  const KEY_ACCESS_TOKEN = 'access_token_key';
+  const KEY_CLIENT_ID = 'client_id';
+  const KEY_CLIENT_SECRET = 'client_secret';
+  const KEY_APP_NAME = 'app_name';
+  const KEY_ENV = 'env';
+  const KEY_TOKEN_URI = 'token_uri';
+
+  const HEADER_PARAM_ACCEPT = "Accept";
+  const HEADER_PARAM_CTYPE = "Content-Type";
+  const HEADER_PARAM_ACCESS_TOKEN = "Authorization";
+  const HEADER_PARAM_USER_AGENT = "User-Agent";
+  const HEADER_VALUE_JSON = "application/json";
+  const HEADER_VALUE_USER_AGENT = "Ezpizee Web/1.0";
+  const HEADER_PARAM_APP_NAME = "App-Name";
+  const HEADER_PARAM_APP_VERSION = "App-Version";
+  const HEADER_VALUE_APP_VERSION = "0.0.1";
+  const HEADER_PARAM_APP_PLATFORM = "App-Platform";
+  const HEADER_VALUE_APP_PLATFORM = "Unknown";
+  const HEADER_PARAM_OS_PLATFORM_VERSION = "OS-Platform-Version";
+  const HEADER_VALUE_OS_PLATFORM_VERSION = "Unknown";
+  const HEADER_LANGUAGE_TAG = "Language-Tag";
+
+  private $isMultipart = false;
+  private $platform = '';
+  private $platformVersion = '';
+
   /**
    * @var Config
    */
@@ -36,7 +62,6 @@ class Client
       $this->config = $config;
       $this->schema = $schema;
       $this->host = $host;
-      $this->setHeaderBase();
     }
     else {
       throw new RuntimeException('Invalid microservices config', 422);
@@ -102,8 +127,21 @@ class Client
     return $this->request($this->url($uri));
   }
 
+  public function getConfig(string $key, $default=null) {return $this->config->get($key, $default);}
+
+  public function getExpireIn(string $tokeyKey): string {
+    return isset($_COOKIE[$tokeyKey.'_ei']) ? $_COOKIE[$tokeyKey.'_ei'] : '0';
+  }
+
+  public function setMultipart(bool $b): void {$this->isMultipart = $b;}
+
+  public function verifyPeer(bool $b): void {Request::verifyPeer($b);}
+
+  public function setPlatform(string $platform): void {$this->platform=$platform;}
+
+  public function setPlatformVersion(string $platformVersion): void {$this->platformVersion=$platformVersion;}
+
   protected function getHeaders(): array {return $this->headers;}
-  protected function getConfig(string $key, $default=null) {return $this->config->get($key, $default);}
   protected function hasHeader(string $key): bool {return isset($this->headers[$key]);}
   protected function url(string $uri): string
   {
@@ -112,7 +150,8 @@ class Client
 
   private function request(string $url): Response
   {
-    $this->requestToken();
+    $this->setBaseHeaders();
+
     $response = new Response([]);
 
     switch ($this->method) {
@@ -142,28 +181,59 @@ class Client
     return $response;
   }
 
-  private function requestToken(): void
+  private function setBaseHeaders(): void
   {
-    if (!$this->hasHeader('Authorization')) {
-      if (!isset($_COOKIE['token'])) {
-        $tokenUri = $this->config->get('token_uri');
-        $response = Request::post($this->url($tokenUri), $this->headers, null, $this->config->get('client_id'), $this->config->get('client_secret'));
-        if (
-          isset($response->body->data)
-          && isset($response->body->data->AuthorizationBearerToken)
-          && isset($response->body->data->expire_in)
-        ) {
-          setcookie('token', $response->body->data->AuthorizationBearerToken, time() + ($response->body->data->expire_in - (10 * 60 * 1000)), "/");
-          $this->addHeader('Authorization', 'Bearer ' . $response->body->data->AuthorizationBearerToken);
-        }
-      } else {
-        $this->addHeader('Authorization', 'Bearer ' . $_COOKIE['token']);
-      }
+    if (!$this->isMultipart && !$this->hasHeader(self::HEADER_PARAM_CTYPE)) {
+      $this->addHeader(self::HEADER_PARAM_CTYPE, self::HEADER_VALUE_JSON);
     }
+    if (!$this->hasHeader(self::HEADER_PARAM_ACCEPT)) {
+      $this->addHeader(self::HEADER_PARAM_ACCEPT, self::HEADER_VALUE_JSON);
+    }
+    if (!$this->hasHeader(self::HEADER_PARAM_USER_AGENT)) {
+      $this->addHeader(self::HEADER_PARAM_USER_AGENT, self::HEADER_VALUE_USER_AGENT);
+    }
+    if (!$this->hasHeader(self::HEADER_PARAM_APP_VERSION)) {
+      $this->addHeader(self::HEADER_PARAM_APP_VERSION, self::HEADER_VALUE_APP_VERSION);
+    }
+    if (!$this->hasHeader(self::HEADER_PARAM_APP_PLATFORM)) {
+      $this->addHeader(self::HEADER_PARAM_APP_PLATFORM, $this->platform?$this->platform:self::HEADER_VALUE_APP_PLATFORM);
+    }
+    if (!$this->hasHeader(self::HEADER_PARAM_OS_PLATFORM_VERSION)) {
+      $this->addHeader(self::HEADER_PARAM_OS_PLATFORM_VERSION, $this->platformVersion?$this->platformVersion:self::HEADER_VALUE_OS_PLATFORM_VERSION);
+    }
+    if (!$this->hasHeader(self::HEADER_PARAM_APP_NAME) && $this->getConfig(self::KEY_APP_NAME)) {
+      $this->addHeader(self::HEADER_PARAM_APP_NAME, $this->getConfig(self::KEY_APP_NAME));
+    }
+    $this->fetchBearerToken($this->getConfig(self::KEY_ACCESS_TOKEN, 'token'));
   }
 
-  private function setHeaderBase(): void
-  {
-    $this->addHeader('App-Name', 'My Electron App Demo');
+  private function fetchBearerToken(string $tokenKey): void {
+    if (!$this->hasHeader(self::HEADER_PARAM_ACCESS_TOKEN) && $this->config->has(self::KEY_TOKEN_URI)) {
+      if (!isset($_COOKIE[$tokenKey])) {
+        if ($this->getConfig(self::KEY_ENV) === 'local') {
+          $this->verifyPeer(false);
+        }
+        $response = Request::post(
+          $this->url($this->getConfig(self::KEY_TOKEN_URI)),
+          $this->getHeaders(),
+          null,
+          $this->getConfig(self::KEY_CLIENT_ID),
+          $this->getConfig(self::KEY_CLIENT_SECRET)
+        );
+        // TODO: wrong app name
+        die($this->config);
+        if (isset($response->body->data)
+          && isset($response->body->data->AuthorizationBearerToken)
+          && isset($response->body->data->expire_in)) {
+          $expire = time() + ($response->body->data->expire_in - (10 * 60 * 1000));
+          setcookie($tokenKey, $response->body->data->AuthorizationBearerToken, $expire, "/");
+          setcookie($tokenKey.'_ei', $response->body->data->expire_in, $expire, "/");
+          $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Bearer ' . $response->body->data->AuthorizationBearerToken);
+        }
+      }
+      else {
+        $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Bearer ' . $_COOKIE[$tokenKey]);
+      }
+    }
   }
 }
