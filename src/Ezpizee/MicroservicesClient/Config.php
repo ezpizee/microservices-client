f88bc30705d552c37f99b819bfe6fd956afece73
Sothea Nim <sothea.nim@ezpizee.com>
2021-01-06 10:52:35 -0500

    formatted

--- src/Ezpizee/MicroservicesClient/Config.php
+++ src/Ezpizee/MicroservicesClient/Config.php
@@ -6,9 +6,9 @@ use Ezpizee\Utils\ListModel;
 
 class Config extends ListModel
 {
-  public function isValid()
-  : bool
-  {
-    return !empty($this->get('client_id')) && !empty($this->get('client_secret')) && !empty($this->get('token_uri'));
-  }
+    public function isValid()
+    : bool
+    {
+        return !empty($this->get('client_id')) && !empty($this->get('client_secret')) && !empty($this->get('token_uri'));
+    }
 }
