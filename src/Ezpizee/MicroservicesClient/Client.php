48ac48346f8acab30730d07a62a380f3ae14f429
Sothea Nim <sothea.nim@ezpizee.com>
2022-03-19 16:32:48 -0400

    improved client

--- src/Ezpizee/MicroservicesClient/Client.php
+++ src/Ezpizee/MicroservicesClient/Client.php
@@ -67,7 +67,7 @@ class Client
             $this->host = $host;
             $this->tokenHandler = $tokenHandler;
             if ($config->has(self::KEY_BEARER)) {
-                $this->addBearerTokenHeader($config->get(self::KEY_BEARER));
+                $this->addAuthorizationHeader($config->get(self::KEY_BEARER));
             }
         }
         else {
@@ -240,7 +240,7 @@ class Client
                 if ($tokenHandler instanceof TokenHandlerInterface) {
                     $token = $tokenHandler->getToken();
                     if ($token instanceof Token && $token->getAuthorizationBearerToken()) {
-                        $this->addBearerTokenHeader($token->getAuthorizationBearerToken());
+                        $this->addAuthorizationHeader($token->getAuthorizationBearerToken());
                     }
                     else {
                         $tokenHandler->setCookie($tokenKey);
@@ -273,7 +273,7 @@ class Client
                         $tokenHandler->setCookie($tokenKey, $cookieVal, $expire);
                         $tokenHandler->keepToken(new Token(json_decode(json_encode($response->body->data), true)));
                     }
-                    $this->addBearerTokenHeader($response->body->data->{$response->body->data->token_param_name});
+                    $this->addAuthorizationHeader($response->body->data->{$response->body->data->token_param_name});
                 }
                 else {
                     throw new RuntimeException(
@@ -388,15 +388,20 @@ class Client
         $this->platformVersion = $platformVersion;
     }
 
-    public function addBearerTokenHeader(string $token)
+    public function addAuthorizationHeader(string $token)
     : void
     {
-        $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Bearer '.$token);
+        if (strpos($token, 'Bearer ') !== false || strpos($token, 'Basic ') !== false) {
+            $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, $token);
+        }
+        else {
+            $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Bearer '. $token);
+        }
     }
 
     public function addBasicTokenHeader(string $user, string $pwd)
     : void
     {
-        $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Basic '.base64_encode($user.':'.$pwd));
+        $this->addAuthorizationHeader('Basic ' . base64_encode($user.':'.$pwd));
     }
 }
